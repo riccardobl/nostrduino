e70d3ebf0ba79b9574a95b3f38ba7c88cc8af372
riccardobl <riccardobl@users.noreply.github.com>
2025-03-07 14:43:09 +0000

    deploy: 5615ffdfb0b591a5e6cdbfe13ba612a08d545015

--- search/variables_a.js
+++ search/variables_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['n_0',['n',['../classnostr_1_1NWCResponseCallbackBase.html#a3ab0eff64ab942a724e9744520f0c964',1,'nostr::NWCResponseCallbackBase']]],
-  ['network_1',['network',['../structnostr_1_1s__GetInfoResponse.html#ab731456e15cf89796232608fd69d81ec',1,'nostr::s_GetInfoResponse']]]
+  ['network_1',['network',['../structnostr_1_1s__GetInfoResponse.html#ab731456e15cf89796232608fd69d81ec',1,'nostr::s_GetInfoResponse']]],
+  ['notifications_2',['notifications',['../structnostr_1_1s__GetInfoResponse.html#a8a2004e32540c472007d8511daafa5d9',1,'nostr::s_GetInfoResponse']]]
 ];
